789953c4f00988f5b082acdd75ae205d27d4bf4d
TakemichiHanagaki <88473676+TakemichiHanagaki@users.noreply.github.com>
2022-08-19 14:58:19 +0530

    Update afk.py

--- Shikimori/modules/afk.py
+++ Shikimori/modules/afk.py
@@ -180,6 +180,7 @@ def __gdpr__(user_id):
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk, run_async=True)
 AFK_REGEX_HANDLER = MessageHandler(Filters.regex("(?i)brb"), afk, friendly="afk", run_async=True)
+AFK_REGEX_HANDLER = MessageHandler(Filters.regex("(?i)afk"), afk, friendly="afk", run_async=True)
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.chat_type.groups, no_longer_afk, run_async=True)
 AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.chat_type.groups, reply_afk, run_async=True)
 
