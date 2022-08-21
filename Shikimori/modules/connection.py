9d2de876866a72580f6b258de59c0b516961ea7a
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 14:26:30 +0530

    Update connection.py

--- Shikimori/modules/connection.py
+++ Shikimori/modules/connection.py
@@ -448,7 +448,7 @@ dispatcher.add_handler(ALLOW_CONNECTIONS_HANDLER)
 dispatcher.add_handler(HELP_CONNECT_CHAT_HANDLER)
 dispatcher.add_handler(CONNECT_BTN_HANDLER)
 
-__mod_name__ = "Connection 🖧"
+__mod_name__ = "「 Connection 」"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
