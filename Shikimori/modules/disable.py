7ebdda17de6efc335b29c5bd10850deb2804024f
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 14:12:33 +0530

    Update disable.py

--- Shikimori/modules/disable.py
+++ Shikimori/modules/disable.py
@@ -370,7 +370,7 @@ else:
     DisableAbleRegexHandler = RegexHandler
     DisableAbleMessageHandler = MessageHandler
 
-__mod_name__ = "Disabling ⃠"
+__mod_name__ = "「 Disabling 」"
 
 __help__ = """
 ❂ `/cmds`*:* check the current status of disabled commands
