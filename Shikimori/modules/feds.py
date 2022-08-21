621fc81eb6b9b62f1744c185e45861deb1742a5c
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 14:21:34 +0530

    Update feds.py

--- Shikimori/modules/feds.py
+++ Shikimori/modules/feds.py
@@ -2429,7 +2429,7 @@ def fed_user_help(update: Update, context: CallbackContext):
     )
 
 
-__mod_name__ = "Federations"
+__mod_name__ = "「 Federations 」"
 
 
 NEW_FED_HANDLER = CommandHandler("newfed", new_fed, run_async=True)
