6bae560350a975fae8b8e8cccaea752db3ce82f2
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 16:42:19 +0530

    Update cust_filters.py

--- Shikimori/modules/cust_filters.py
+++ Shikimori/modules/cust_filters.py
@@ -656,7 +656,7 @@ doin?
 Check /markdownhelp to know more!
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "「 Filters 」"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
