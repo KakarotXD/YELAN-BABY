7ed7f562f19b2f28ac1d0373469a9f5161d41a0a
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 16:56:45 +0530

    Update wallpaper.py

--- Shikimori/modules/wallpaper.py
+++ Shikimori/modules/wallpaper.py
@@ -56,7 +56,7 @@ def wall(update: Update, context: CallbackContext):
 WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall, run_async=True)
 dispatcher.add_handler(WALLPAPER_HANDLER)
 
-__mod_name__ = "Wallpaper"
+__mod_name__ = "「 Wallpaper 」"
 __help__ = """
 *Wallpaper*
  ❍ `/wall` : Search for wallpaper of given query
