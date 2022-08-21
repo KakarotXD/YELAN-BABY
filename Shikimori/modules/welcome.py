6977a3e94f438ee946ebebfec7e5b300e605b379
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 15:46:26 +0530

    Update welcome.py

--- Shikimori/modules/welcome.py
+++ Shikimori/modules/welcome.py
@@ -1376,7 +1376,7 @@ dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 dispatcher.add_handler(CAPTCHA_BUTTON_VERIFY_HANDLER)
 
-__mod_name__ = "Greetings 👋"
+__mod_name__ = "「 Greetings 」"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
