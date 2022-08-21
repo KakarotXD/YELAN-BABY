298290ba4ef94f9f1a4f0d2ef8981e8ce5bcf72b
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 16:22:12 +0530

    Update inlinebot.py

--- Shikimori/modules/inlinebot.py
+++ Shikimori/modules/inlinebot.py
@@ -94,7 +94,7 @@ I'm more efficient when added as group admin. By the way these commands can be u
 ➛ pokedex [TEXT]: Pokemon Search
 """
 
-__MODULE__ = "Inline"
+__MODULE__ = "「 Inline 」"
 __HELP__ = f"""
  ==>> **INLINE BOT SERVICE OF @{BOT_USERNAME}** <<==
 `I'm more efficient when added as group admin. By the way these commands can be used by anyone in a group via inline.`
