28df9848f912183c929083778ca05e9187dc4832
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 16:52:19 +0530

    Update feds.py

--- Shikimori/modules/feds.py
+++ Shikimori/modules/feds.py
@@ -2497,3 +2497,17 @@ dispatcher.add_handler(DELETEBTN_FED_HANDLER)
 dispatcher.add_handler(FED_OWNER_HELP_HANDLER)
 dispatcher.add_handler(FED_ADMIN_HELP_HANDLER)
 dispatcher.add_handler(FED_USER_HELP_HANDLER)
+
+_help_ = """
+*Federation*
+Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
+But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
+*No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
+You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
+*Commands:*\n
+Feds are now divided into 3 sections for your ease.
+• /fedownerhelp*:* Provides help for fed creation and owner only commands
+• /fedadminhelp*:* Provides help for fed administration commands
+• /feduserhelp*:* Provides help for commands anyone can use
+"""
+_mod_name_= "「 Federation 」"
