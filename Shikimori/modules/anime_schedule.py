2ba9485212868e393e40cc73aa46a0dc8f23979e
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 13:26:30 +0530

    Update anime_schedule.py

--- Shikimori/modules/anime_schedule.py
+++ Shikimori/modules/anime_schedule.py
@@ -47,7 +47,7 @@ def schedule(_, message):
     message.reply_text(f"**Today's Schedule:**\nTime-Zone: Tokyo (GMT +9)\n\n{text}")
 
 
-__mod_name__ = "Schedule"
+__mod_name__ = "「 Anime Schedule 」"
 
 __help__ = """
  ❍ `/latest`: to see latest anime episode
