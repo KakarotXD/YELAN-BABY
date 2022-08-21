686da97bd17b766dc9578a23317951aed98f40fd
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 13:04:28 +0530

    Update afk.py

--- Shikimori/modules/afk.py
+++ Shikimori/modules/afk.py
@@ -190,9 +190,10 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK 💤"
+__mod_name__ = "「 AFK 」"
 __help__ = """
 *AFK*
- ❍ `/afk or afk ` :Tells other users that you are AFK\n
+ ❍ `/later` :Tells other users that you are AFK\n
+ ❍ `/afk` or `afk` :Tells other users that you are AFK\n
  ❍ `brb` :Tells other users that you are busy right now
 """
