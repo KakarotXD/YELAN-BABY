8a1fb3f5005cb906a8debb281bd5be06f1b4a52d
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 15:15:42 +0530

    Update notes.py

--- Shikimori/modules/notes.py
+++ Shikimori/modules/notes.py
@@ -601,7 +601,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
  *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
 """
 
-__mod_name__ = "Notes 📚"
+__mod_name__ = "「 Notes 」"
 
 GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
