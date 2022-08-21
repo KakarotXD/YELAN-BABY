2e3770aa3c2b043938e24b6cf81c919c469791d5
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 13:39:57 +0530

    Update animesitesearch.py

--- Shikimori/modules/animesitesearch.py
+++ Shikimori/modules/animesitesearch.py
@@ -134,7 +134,7 @@ __help__ = """
 • `/kayo`*:* Find anime from animekayo website.
 """
     
-__mod_name__ = "Anime Site Search"
+__mod_name__ = "「 Anime Site 」"
 KAYO_SEARCH_HANDLER = DisableAbleCommandHandler("kayo", kayo, run_async = True)
 ANIMESPOT_SEARCH_HANDLER = DisableAbleCommandHandler("animespot", animespot, run_async = True)
 ANIMETM_SEARCH_HANDLER = DisableAbleCommandHandler("animetm", animetm, run_async = True)
