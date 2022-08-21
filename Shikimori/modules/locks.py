059000e3a53d4946d5407c9d8b3847218595656b
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 15:10:15 +0530

    Update locks.py

--- Shikimori/modules/locks.py
+++ Shikimori/modules/locks.py
@@ -584,7 +584,7 @@ Locking bots will stop non-admins from adding bots to the chat.
 ❂ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "Locks 🔒"
+__mod_name__ = "「 Locks 」"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler(
