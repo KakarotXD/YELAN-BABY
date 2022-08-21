5aed1e0e2e55fe8045c06a00a844f6d3e7432d45
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-21 13:58:50 +0530

    Update backups.py

--- Shikimori/modules/backups.py
+++ Shikimori/modules/backups.py
@@ -406,7 +406,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
+__mod_name__ = "「 Backups 」"
 
 IMPORT_HANDLER = CommandHandler("import", import_data, run_async=True)
 EXPORT_HANDLER = CommandHandler(
